bf89c1cad84fddf3bc3a5a4919f9d98a31023611
James Leahy <defuncart@gmail.com>
2020-10-03 14:16:30 +0200

    Update HomeTab to display levels and learn button.

--- lib/widgets/home_screen/home_tab/home_tab.dart
+++ lib/widgets/home_screen/home_tab/home_tab.dart
@@ -1,30 +1,42 @@
-import 'package:bratacha/extensions/country_extensions.dart';
 import 'package:bratacha/intl/localizations.dart';
-import 'package:bratacha/modules/country_database/country_database.dart';
-import 'package:bratacha/widgets/common/flag.dart';
+import 'package:bratacha/managers/level_manager.dart';
+import 'package:bratacha/widgets/learn_screen/learn_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final countries = CountryService.countries;
+    final levelManager = context.repository<LevelManager>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.homeTabLabelText),
       ),
       body: SafeArea(
-        child: ListView.separated(
-          separatorBuilder: (_, __) => Divider(),
-          itemCount: countries.length,
-          itemBuilder: (_, index) => ListTile(
-            leading: Flag(
-              countries[index].key,
-              height: 100,
-              width: 100,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  children: [
+                    for (var i = 0; i < levelManager.numberLevels; i++)
+                      ElevatedButton(
+                        onPressed: null,
+                        child: Text(AppLocalizations.homeTabLevelButtonText(level: i + 1)),
+                      ),
+                  ],
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pushNamed(LearnScreen.routeName),
+                  child: Text(AppLocalizations.homeTabLeanFlagsButtonText),
+                ),
+              ],
             ),
-            title: Text(countries[index].localizedName),
           ),
         ),
       ),
