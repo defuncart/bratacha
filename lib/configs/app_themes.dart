9220ab48185449b2326614ad68b961c815c455f5
James Leahy <defuncart@gmail.com>
2020-10-04 09:50:21 +0200

    Add ui color theme.

--- lib/configs/app_themes.dart
+++ lib/configs/app_themes.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 abstract class AppThemes {
-  static final light = ThemeData.light();
+  static final theme = ThemeData.dark().copyWith(
+    accentColor: _AppColors.accent,
+    scaffoldBackgroundColor: _AppColors.primary,
+    appBarTheme: AppBarTheme(
+      color: _AppColors.primaryDark,
+    ),
+  );
+}
 
-  static final dark = ThemeData.dark();
+class _AppColors {
+  static const primary = Color(0xff3A4750);
+  static const primaryDark = Color(0xff303841);
+  static const accent = Color(0xff00adb5);
+  static const text = Color(0xffeeeeee);
 }
