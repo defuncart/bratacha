b710a4cf0316566a78b2668be393173806ef6555
James Leahy <defuncart@gmail.com>
2020-10-04 09:14:00 +0200

    Update GameScreen.

--- lib/widgets/game_screen/game_screen.dart
+++ lib/widgets/game_screen/game_screen.dart
@@ -24,7 +24,6 @@ class GameScreen extends StatefulWidget {
 
 class _GameScreenState extends State<GameScreen> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-
   var _isLoading = true;
   GameService _gameService;
 
@@ -52,41 +51,53 @@ class _GameScreenState extends State<GameScreen> {
               child: Center(
                 child: Column(
                   children: [
-                    Text(
-                      _gameService.questionCountry.localizedName,
-                      style: Theme.of(context).textTheme.headline3,
-                      textAlign: TextAlign.center,
-                    ),
+                    SizedBox(height: 16.0),
                     Expanded(
-                      child: Wrap(
-                        spacing: 16.0,
-                        runSpacing: 16.0,
-                        children: [
-                          for (final id in _gameService.answerCountries)
-                            GestureDetector(
-                              child: Flag(
-                                id,
-                                width: MediaQuery.of(context).size.width * 0.4,
-                                height: MediaQuery.of(context).size.width * 0.4,
-                              ),
-                              onTap: () {
-                                final correct = _gameService.answerWithId(id);
-                                _scaffoldKey.currentState.showSnackBar(
-                                  SnackBar(
-                                    content: Icon(correct ? Icons.check : Icons.close),
-                                    duration: Duration(seconds: 1),
-                                  ),
-                                );
-                                if (_gameService.levelCompleted) {
-                                  Navigator.of(context).pop();
-                                } else {
-                                  setState(() {});
-                                }
-                              },
-                            )
-                        ],
+                      child: Center(
+                        child: Text(
+                          _gameService.questionCountry.localizedName,
+                          style: Theme.of(context).textTheme.headline3,
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    )
+                    ),
+                    LayoutBuilder(
+                      builder: (_, constraints) {
+                        final size = (constraints.maxWidth - 16.0 - 16.0 - 16.0) / 2;
+
+                        return Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Wrap(
+                            spacing: 16.0,
+                            runSpacing: 16.0,
+                            children: [
+                              for (final id in _gameService.answerCountries)
+                                GestureDetector(
+                                  child: Flag(
+                                    id,
+                                    width: size,
+                                    height: size,
+                                  ),
+                                  onTap: () {
+                                    final correct = _gameService.answerWithId(id);
+                                    _scaffoldKey.currentState.showSnackBar(
+                                      SnackBar(
+                                        content: Icon(correct ? Icons.check : Icons.close),
+                                        duration: Duration(seconds: 1),
+                                      ),
+                                    );
+                                    if (_gameService.levelCompleted) {
+                                      Navigator.of(context).pop();
+                                    } else {
+                                      setState(() {});
+                                    }
+                                  },
+                                ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ],
                 ),
               ),
