43359917308fc9aa4b787fdc18f25528eeec2963
James Leahy <defuncart@gmail.com>
2020-10-06 17:55:01 +0200

    Feature/ui design single theme (#6)
    Adds a single ui theme for the app. Removes obsolete logic for isDarkMode. Constrains Flutter to >= 1.22 and dart >= 2.10.
    
    Commits:
    * Remove dark mode & light mode support.
    * Add ui color theme.
    * Update BottomNavigationBar styling.
    * Update iOS LaunchScreen color.
    * Bump min iOS version to 9.0, remove LaunchImage.
    * Update iOS StatusBar color to be light.
    * Add Android splash color.
    * Update HomeTab styling.
    * Set app bar's elevation to 0.
    * Constrain dart, flutter sdks to latest versions.

--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -7,7 +7,6 @@ import 'package:bratacha/managers/level_manager.dart';
 import 'package:bratacha/modules/settings_database/settings_database.dart';
 import 'package:bratacha/widgets/game_screen/game_screen.dart';
 import 'package:bratacha/widgets/home_screen/home_screen.dart';
-import 'package:bratacha/widgets/home_screen/settings_tab/dark_mode_cubit.dart';
 import 'package:bratacha/widgets/home_screen/settings_tab/language_cubit.dart';
 import 'package:bratacha/widgets/learn_screen/learn_screen.dart';
 import 'package:flutter/material.dart';
@@ -47,9 +46,6 @@ class _MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<DarkModeCubit>(
-          create: (_) => DarkModeCubit(context.repository<ISettingsDatabase>()),
-        ),
         BlocProvider<LanguageCubit>(
           create: (_) => LanguageCubit(context.repository<ISettingsDatabase>()),
         ),
@@ -60,35 +56,31 @@ class _MyApp extends StatelessWidget {
 }
 
 class __MyApp extends StatelessWidget {
-  const __MyApp({
-    Key key,
-  }) : super(key: key);
+  const __MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DarkModeCubit, bool>(
-      builder: (_, isDarkMode) => BlocBuilder<LanguageCubit, String>(
-        builder: (_, language) => MaterialApp(
-          localizationsDelegates: [
-            const AppLocalizationsDelegate(),
-            const CountryLocalizationsDelegate(),
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-            CyMaterialLocalizations.delegate,
-            GaMaterialLocalizations.delegate,
-          ],
-          locale: Locale(language),
-          supportedLocales: AppLocalizationsDelegate.supportedLocals,
-          theme: AppThemes.light,
-          darkTheme: AppThemes.dark,
-          themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-          home: HomeScreen(),
-          routes: {
-            LearnScreen.routeName: (_) => LearnScreen(),
-            GameScreen.routeName: (_) => GameScreen(),
-          },
-        ),
+    return BlocBuilder<LanguageCubit, String>(
+      builder: (_, language) => MaterialApp(
+        localizationsDelegates: [
+          const AppLocalizationsDelegate(),
+          const CountryLocalizationsDelegate(),
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          CyMaterialLocalizations.delegate,
+          GaMaterialLocalizations.delegate,
+        ],
+        locale: Locale(language),
+        supportedLocales: AppLocalizationsDelegate.supportedLocals,
+        theme: AppThemes.theme,
+        darkTheme: AppThemes.theme,
+        themeMode: ThemeMode.dark,
+        home: HomeScreen(),
+        routes: {
+          LearnScreen.routeName: (_) => LearnScreen(),
+          GameScreen.routeName: (_) => GameScreen(),
+        },
       ),
     );
   }
