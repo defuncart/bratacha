711386d6c6a9f2c14c0cfd85ae42f9bbb0ff1c57
James Leahy <defuncart@gmail.com>
2020-10-07 15:17:26 +0200

    Simplify GameScreen builders.

--- lib/widgets/game_screen/game_screen.dart
+++ lib/widgets/game_screen/game_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:bratacha/managers/level_manager.dart';
 import 'package:bratacha/modules/settings_database/settings_database.dart';
 import 'package:bratacha/services/game_service.dart';
@@ -16,62 +14,45 @@ class GameScreenArguments {
   const GameScreenArguments({@required this.level});
 }
 
-class GameScreen extends StatefulWidget {
+class GameScreen extends StatelessWidget {
   static const routeName = 'GameScreen';
 
   const GameScreen({Key key}) : super(key: key);
 
   @override
-  _GameScreenState createState() => _GameScreenState();
-}
-
-class _GameScreenState extends State<GameScreen> {
-  var _isLoading = true;
-  GameService _gameService;
-
-  @override
-  void initState() {
-    super.initState();
-
-    scheduleMicrotask(() {
-      final args = ModalRoute.of(context).settings.arguments as GameScreenArguments;
-      final level = args.level;
-      _gameService = GameService(
-        isHardDifficulty: context.repository<ISettingsDatabase>().isHardDifficulty,
-        level: level,
-        levelManager: context.repository<LevelManager>(),
-      );
-      _gameService.initialize();
-      setState(() => _isLoading = false);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context).settings.arguments as GameScreenArguments;
+    final level = args.level;
+
     return Scaffold(
       appBar: AppBar(),
-      body: _isLoading
-          ? Container()
-          : RepositoryProvider<IGameService>(
-              create: (_) => _gameService,
-              child: MultiBlocProvider(
-                providers: [
-                  BlocProvider<QuestionCubit>(
-                    create: (_) => QuestionCubit(gameService: _.repository<IGameService>()),
-                  ),
-                  BlocProvider<AnswersCubit>(
-                    create: (_) => AnswersCubit(gameService: _.repository<IGameService>()),
-                  ),
-                ],
-                child: Builder(
-                  builder: (_) => SafeArea(
-                    child: Center(
-                      child: QuestionAnswerPanel(),
-                    ),
-                  ),
-                ),
+      body: RepositoryProvider<IGameService>(
+        create: (_) => GameService(
+          isHardDifficulty: context.repository<ISettingsDatabase>().isHardDifficulty,
+          level: level,
+          levelManager: context.repository<LevelManager>(),
+        )..initialize(),
+        lazy: false,
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<QuestionCubit>(
+              create: (contextRepository) => QuestionCubit(
+                gameService: contextRepository.repository<IGameService>(),
               ),
             ),
+            BlocProvider<AnswersCubit>(
+              create: (contextRepository) => AnswersCubit(
+                gameService: contextRepository.repository<IGameService>(),
+              ),
+            ),
+          ],
+          child: SafeArea(
+            child: Center(
+              child: QuestionAnswerPanel(),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
