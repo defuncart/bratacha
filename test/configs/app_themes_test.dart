6c30864ba121f967421789a69370856d43ce692a
James Leahy <defuncart@gmail.com>
2020-10-12 15:33:40 +0200

    Add tests for AppThemes.

---
+++ test/configs/app_themes_test.dart
@@ -0,0 +1,8 @@
+import 'package:bratacha/configs/app_themes.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('theme', () {
+    expect(AppThemes.theme, isNotNull);
+  });
+}
