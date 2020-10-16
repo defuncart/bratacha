ca1cdc07900ad5133f59fb06305a7157eee5ffce
James Leahy <defuncart@gmail.com>
2020-10-16 15:57:25 +0200

    Update LanguagePanel to use LanguageButton.

--- lib/widgets/common/panels/language_panel/language_panel.dart
+++ lib/widgets/common/panels/language_panel/language_panel.dart
@@ -1,68 +1,50 @@
 import 'package:bratacha/intl/localizations.dart';
+import 'package:bratacha/widgets/common/panels/language_panel/langauge_button.dart';
 import 'package:bratacha/widgets/common/panels/language_panel/language_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LanguagePanel extends StatelessWidget {
+  final bool showTextLabel;
+
   const LanguagePanel({
     Key key,
+    this.showTextLabel = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(AppLocalizations.settingsTabLanguageLabel),
-        BlocBuilder<LanguageCubit, String>(
-          builder: (_, language) => Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              for (final locale in AppLocalizationsDelegate.supportedLocals)
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                  child: _LanguageButton(
-                    label: locale.languageCode,
-                    isSelected: locale.languageCode == language,
-                    onPressed: () => context.bloc<LanguageCubit>().setLanguage(locale.languageCode),
-                  ),
-                )
-            ],
+        if (showTextLabel)
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(AppLocalizations.settingsTabLanguageLabel),
           ),
+        LayoutBuilder(
+          builder: (_, constraints) {
+            final languageCodes = AppLocalizationsDelegate.supportedLocals.map((locale) => locale.languageCode);
+            final size = (constraints.maxWidth * 0.75) / languageCodes.length;
+
+            return BlocBuilder<LanguageCubit, String>(
+              builder: (_, language) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  for (final languageCode in languageCodes)
+                    LanguageButton(
+                      languageCode: languageCode,
+                      isSelected: languageCode == language,
+                      onPressed: () => context.bloc<LanguageCubit>().setLanguage(languageCode),
+                      size: size,
+                    )
+                ],
+              ),
+            );
+          },
         ),
       ],
-    );
-  }
-}
-
-class _LanguageButton extends StatelessWidget {
-  final String label;
-  final bool isSelected;
-  final void Function() onPressed;
-
-  const _LanguageButton({
-    Key key,
-    @required this.label,
-    @required this.isSelected,
-    @required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Container(
-        color: isSelected ? Theme.of(context).accentColor : Theme.of(context).accentColor.withAlpha(128),
-        child: Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: Text(
-            label.toUpperCase(),
-            style: TextStyle(
-              color: Theme.of(context).scaffoldBackgroundColor,
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
