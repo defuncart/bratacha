43003dde3ddc654306362641f75ef8f0d06cfaae
James Leahy <defuncart@gmail.com>
2020-10-18 13:41:30 +0200

    Add CustomDialogRequest.

--- lib/modules/dialog_manager/dialog_manager.dart
+++ lib/modules/dialog_manager/dialog_manager.dart
@@ -2,6 +2,7 @@ library dialog_manager;
 
 export 'src/enums/confirm_dialog_response_type.dart';
 export 'src/models/confirm_dialog_request.dart';
+export 'src/models/custom_dialog_request.dart';
 export 'src/models/informative_dialog_request.dart';
 export 'src/services/dialog_service.dart';
 export 'src/services/i_dialog_service.dart';
