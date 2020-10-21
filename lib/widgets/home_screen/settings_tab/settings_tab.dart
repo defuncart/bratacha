cab5330820e92c6e1daa186b4d30934bd69eefaf
James Leahy <defuncart@gmail.com>
2020-10-21 17:21:08 +0200

    Ensure SettingsTab rebuilds when language changed.

--- lib/widgets/home_screen/settings_tab/settings_tab.dart
+++ lib/widgets/home_screen/settings_tab/settings_tab.dart
@@ -4,6 +4,7 @@ import 'package:bratacha/modules/player_data/player_data.dart';
 import 'package:bratacha/services/url_launcher_service/i_url_launcher_service.dart';
 import 'package:bratacha/widgets/common/buttons/custom_elevated_button.dart';
 import 'package:bratacha/widgets/common/panels/hard_difficulty_panel/hard_difficulty_panel.dart';
+import 'package:bratacha/widgets/common/panels/language_panel/language_cubit.dart';
 import 'package:bratacha/widgets/common/panels/language_panel/language_panel.dart';
 import 'package:bratacha/widgets/home_screen/settings_tab/feedback_panel.dart';
 import 'package:bratacha/widgets/home_screen/settings_tab/settings_popup_menu_button.dart';
@@ -15,46 +16,49 @@ class SettingsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.settingsTabLabelText),
-        actions: [
-          SettingsPopupMenuButton(parentContext: context),
-        ],
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 16.0),
-              Text(AppLocalizations.settingsTabLanguageLabel),
-              SizedBox(height: 8.0),
-              LanguagePanel(),
-              HardDifficultyPanel(),
-              Center(
-                child: CustomElevatedButton(
-                  onPressed: () async {
-                    final response = await context.repository<IDialogService>().requestConfirmDialog(
-                          ConfirmDialogRequest(
-                            title: AppLocalizations.resetProgressDialogTitle,
-                            description: AppLocalizations.resetProgressDialogDescription,
-                            negativeButtonText: AppLocalizations.generalNo,
-                            positiveButtonText: AppLocalizations.generalYes,
-                          ),
-                        );
-                    if (response.isPositive) {
-                      await context.repository<IPlayerDataService>().reset();
-                    }
-                  },
-                  buttonText: AppLocalizations.settingsTabResetProgressLabel,
+    // whole widget should be rebuilt when language is changed
+    return BlocBuilder<LanguageCubit, String>(
+      builder: (_, __) => Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.settingsTabLabelText),
+          actions: [
+            SettingsPopupMenuButton(parentContext: context),
+          ],
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 16.0),
+                Text(AppLocalizations.settingsTabLanguageLabel),
+                SizedBox(height: 8.0),
+                LanguagePanel(),
+                HardDifficultyPanel(),
+                Center(
+                  child: CustomElevatedButton(
+                    onPressed: () async {
+                      final response = await context.repository<IDialogService>().requestConfirmDialog(
+                            ConfirmDialogRequest(
+                              title: AppLocalizations.resetProgressDialogTitle,
+                              description: AppLocalizations.resetProgressDialogDescription,
+                              negativeButtonText: AppLocalizations.generalNo,
+                              positiveButtonText: AppLocalizations.generalYes,
+                            ),
+                          );
+                      if (response.isPositive) {
+                        await context.repository<IPlayerDataService>().reset();
+                      }
+                    },
+                    buttonText: AppLocalizations.settingsTabResetProgressLabel,
+                  ),
                 ),
-              ),
-              FeedbackPanel(
-                urlLaucherService: context.repository<IUrlLaucherService>(),
-              ),
-            ],
+                FeedbackPanel(
+                  urlLaucherService: context.repository<IUrlLaucherService>(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
