d6018dd7f5dfbf823c8d2bbdb4fc24527b628df6
James Leahy <defuncart@gmail.com>
2020-10-21 15:50:18 +0200

    Add IUrlLaucherService.

---
+++ test/services/url_launcher_service/url_launcher_service_test.dart
@@ -0,0 +1,13 @@
+import 'package:bratacha/services/url_launcher_service/url_launcher_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final urlLaucherService = UrlLaucherService();
+
+  testWidgets('UrlLaucherService', (tester) async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    // ignore: unawaited_futures
+    urlLaucherService.openUrl('test');
+  });
+}
