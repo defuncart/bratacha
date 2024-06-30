3fce2deab5e79a36f3b6a09eb4e0303507fa7bf5
James Leahy <defuncart@gmail.com>
2024-06-30 09:50:14 +0200

    FlagTab basic tests

---
+++ test/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:bratacha/modules/player_data/src/services/i_player_data_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockPlayerDataService extends Mock implements IPlayerDataService {}
