d9e8c8f63d47cf99e1806fa853e4282d7f284d02
James Leahy <defuncart@gmail.com>
2024-07-07 12:39:32 +0200

    Scripts add offline

--- bin/test_coverage_local.sh
+++ bin/test_coverage_local.sh
@@ -1,6 +1,6 @@
 dart run dart_code_coverage:generate_dart_file -r ".*\.g\.dart","localizations.dart"
 
-flutter test --coverage --exclude-tags app_store_screenshots
+flutter test --no-pub --coverage --exclude-tags app_store_screenshots
 
 dart run dart_code_coverage:edit_lcov -r ".*\.g\.dart","localizations.dart","app_icon.dart"
 
