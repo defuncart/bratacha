16b423b021fe884085bd8e265d510060d1f13254
James Leahy <defuncart@gmail.com>
2025-11-16 10:33:55 +0100

    Migrate l10n

--- lib/intl/localizations.dart
+++ lib/intl/localizations.dart
@@ -2,5 +2,5 @@ import 'package:bratacha/intl/generated/localizations.dart';
 import 'package:flutter/material.dart';
 
 extension L10nExtension on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
+  AppLocalizations get l10n => AppLocalizations.of(this);
 }
